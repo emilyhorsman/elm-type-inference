ce71fea345d7a40183464a5176d55eb94d6f6a17
Emily Horsman <me@emilyhorsman.com>
2018-10-14 00:55:00 -0400

    Starting some simple parser tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+
+type Parser = Parsec Void String
+
+
+bool :: Parser Bool
+bool =
+    True <$ string "True" <|> False <$ string "False"
+
+
+escapedSingleQuote :: Parser Char
+escapedSingleQuote =
+    '\'' <$ string "\\'"
+
+
+singleChar :: Parser Char
+singleChar =
+    between surround surround $ try escapedSingleQuote <|> noneOf "'"
+    where
+        surround = char '\''
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,28 @@
+import Test.Hspec
+import Test.Hspec.Megaparsec
+import Text.Megaparsec
+
+import Lib
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "bool" $ do
+        it "returns True" $ do
+            parse bool "" "True" `shouldParse` True
+
+        it "returns False" $ do
+            parse bool "" "False" `shouldParse` False
+
+        it "fails" $ do
+            parse bool "" `shouldFailOn` "asdf"
+
+    describe "singleChar" $ do
+        it "returns escaped char" $ do
+            parse singleChar "" "'\''" `shouldParse` '\''
+
+        it "returns char" $ do
+            parse singleChar "" "'a'" `shouldParse` 'a'
+
+        it "fails on unescaped quote" $ do
+            parse singleChar "" `shouldFailOn` "'''"
