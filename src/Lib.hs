180aa443a0083020c8fe3e403378ad7e2cf3f390
Emily Horsman <me@emilyhorsman.com>
2018-10-20 11:33:52 -0400

    minor cleanup with escapedChar

--- src/Lib.hs
+++ src/Lib.hs
@@ -138,30 +138,30 @@ bool =
 
 -- TODO: #1 Handle Unicode code point \u{03BB}
 escapedChar :: Parser Char
-escapedChar = choice
-    [ '\n' <$ char 'n'
-    , '\r' <$ char 'r'
-    , '\t' <$ char 't'
-    , '"' <$ char '"'
-    , '\'' <$ char '\''
-    , '\\' <$ char '\\'
-    ] <?> "valid escape sequence: \\n, \\r, \\t, \\\", \\', \\\\"
+escapedChar =
+    char '\\' >> choice
+        [ '\n' <$ char 'n'
+        , '\r' <$ char 'r'
+        , '\t' <$ char 't'
+        , '"' <$ char '"'
+        , '\'' <$ char '\''
+        , '\\' <$ char '\\'
+        ] <?> "valid escape sequence: \\n, \\r, \\t, \\\", \\', \\\\"
 
 
 singleChar :: Parser Char
 singleChar =
-    surroundedBy (char '\'') $
-        (char '\\' >> escapedChar) <|> noneOf "'\\"
+    surroundedBy (char '\'') $ escapedChar <|> noneOf "'\\"
 
 
 singleLineString :: Parser String
 singleLineString =
     surroundedBy (char '"') $
-        many $ (char '\\' >> escapedChar) <|> noneOf "\"\\\r\n"
+        many $ escapedChar <|> noneOf "\"\\\r\n"
 
 
 multiLineString :: Parser String
 multiLineString =
-    surround >> manyTill ((char '\\' >> escapedChar) <|> noneOf "\\") surround
+    surround >> manyTill (escapedChar <|> noneOf "\\") surround
     where
         surround = count 3 (char '"')
