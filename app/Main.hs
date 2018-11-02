b35ab1581aca216d07d8173d4f83006190e3ab21
Emily Horsman <me@emilyhorsman.com>
2018-11-02 14:40:28 -0400

    Add parsing to Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
+import System.Environment (getArgs)
+import Text.Megaparsec
+import Text.Pretty.Simple (pPrint)
+
 import Lib
 
+
 main :: IO ()
-main = return ()
+main = do
+    file <- head <$> getArgs
+    source <- readFile file
+    -- Modified from Text.Megaparsec (parseTest) to print error but pretty
+    -- print AST.
+    case parse topLevelProgram file source of
+        Left e -> putStr (errorBundlePretty e)
+        Right x -> pPrint x
